624c2a7e524b4742f32a732e9ec9bfee4d5069e1
Alistair King <alistair@getlocalmeasure.com>
2023-01-20 08:54:37 +1100

    Adds preact support

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import MsalReactTester from "./MsalReactTester";
+import MsalPreactTester from "./MsalPreactTester";
 import { MsalReactTesterPlugin } from "./MsalReactTesterPlugin";
-export { MsalReactTester, MsalReactTesterPlugin };
+export { MsalReactTester, MsalPreactTester, MsalReactTesterPlugin };
 
